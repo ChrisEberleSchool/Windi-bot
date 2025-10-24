d8d5af7f034420196c6d0e7f88e9424fd3730e79
ChrisEberleSchool <ceber042@mtroyal.ca>
2025-10-23 20:26:37 -0600

    Template for esp32 project start
    credentials update
    
    git credentials3
    
    git credentials4
    
    credentials5

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
 
-// put function declarations here: Startup
+// put function declarations here:
 int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
+  // Start the serial connection
+  Serial.begin(115200);
+  delay(1000); 
+
+  Serial.println("Hello, World!"); 
+
   int result = myFunction(2, 3);
+  Serial.print("Result of myFunction(2, 3): ");
+  Serial.println(result);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // put your main code here, to run repeatedly: ..... test test
 }
 
-// put function definitions here:
+// put function definitions here: example
 int myFunction(int x, int y) {
   return x + y;
 }
